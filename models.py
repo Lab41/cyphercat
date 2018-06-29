df90504a1542d75befc80094a44838d8c69da32c
Lucas Tindall <ltindall@ucsd.edu>
2018-06-29 23:24:07 +0000

    Added models.py which includes a basic multilayer perceptron.

---
+++ models.py
@@ -0,0 +1,67 @@
+from torch import nn 
+import torch.nn.functional as F
+
+
+
+class mlp(nn.Module): 
+    def __init__(self, in_channels, out_channels, n_filters): 
+        super(mlp, self).__init__()
+        
+        self.n_filters = n_filters 
+        
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels, n_filters, kernel_size=3, padding=1), 
+            nn.BatchNorm2d(n_filters), 
+            nn.ReLU(inplace=True), 
+            nn.MaxPool2d(2)
+        ) 
+        # shape = [Batch_size, n_filters, height/2, width/2]
+            
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(n_filters, n_filters*2, kernel_size=3, padding=1), 
+            nn.BatchNorm2d(n_filters*2), 
+            nn.ReLU(inplace=True), 
+            nn.MaxPool2d(2)
+        ) 
+        # shape = [Batch_size, n_filters*2, height/4, width/4] 
+        
+        self.dense_block_1 = nn.Sequential(
+            nn.Linear(n_filters*2 * 8 * 8, 64), 
+            nn.BatchNorm1d(64), 
+            nn.ReLU(inplace=True)
+        ) 
+        # shape = [Batch_size, 64]
+        
+        self.dense_block_2 = nn.Sequential(
+            nn.Linear(64, 32), 
+            nn.BatchNorm1d(32), 
+            nn.ReLU(inplace=True)
+        ) 
+        # shape = [Batch_size, 32]
+        
+        self.dense_block_3 = nn.Sequential( 
+            nn.Linear(32, out_channels), 
+            nn.BatchNorm1d(out_channels)
+        ) 
+        # shape = [Batch_size, 10]
+        
+        
+    def forward(self, x): 
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+
+        x = x.view(-1, self.n_filters*2 * 8 * 8)
+        x = self.dense_block_1(x)
+        x = self.dense_block_2(x)
+        out = self.dense_block_3(x)
+
+        return out
+        
+
+            
+def weights_init(m): 
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
