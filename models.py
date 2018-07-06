2fadf6cd0b62c3b3b4935abf5a37beb4fd7c8571
Lucas Tindall <ltindall@ucsd.edu>
2018-07-06 21:03:15 +0000

    Added adversary 1 and 3 from ML-Leaks paper.

--- models.py
+++ models.py
@@ -2,10 +2,55 @@ from torch import nn
 import torch.nn.functional as F
 
 
+class mlleaks_cnn(nn.Module): 
+    def __init__(self, n_in=3, n_out=10, n_hidden=128): 
+        super(mlleaks_cnn, self).__init__()
+        
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(n_in, 64, kernel_size=5, stride=1, padding=2), 
+            nn.BatchNorm2d(64), 
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2), 
+            nn.BatchNorm2d(128), 
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        ) 
+        self.fc = nn.Linear(128 * 8 * 8, 128)
+        self.output = nn.Linear(128, n_out)
+        
+    def forward(self, x): 
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = x.view(-1, 128 * 8 * 8)
+        x = self.fc(x)
+        out = self.output(x)
+        
+        return out
+    
+class mlleaks_mlp(nn.Module): 
+    def __init__(self, n_in=3, n_out=1, n_hidden=64): 
+        super(mlleaks_mlp, self).__init__()
+        
+        self.hidden = nn.Linear(n_in, n_hidden)
+        self.output = nn.Linear(n_hidden, n_out)
+        
+    def forward(self, x): 
+        x = F.sigmoid(self.hidden(x))
+        out = F.sigmoid(self.output(x))
+        
+        return out
+    
+    
+    
+        
+        
 
-class mlp(nn.Module): 
+class cnn(nn.Module): 
     def __init__(self, in_channels, out_channels, n_filters): 
-        super(mlp, self).__init__()
+        super(cnn, self).__init__()
         
         self.n_filters = n_filters 
         
@@ -26,9 +71,10 @@ class mlp(nn.Module):
         # shape = [Batch_size, n_filters*2, height/4, width/4] 
         
         self.dense_block_1 = nn.Sequential(
-            nn.Linear(n_filters*2 * 8 * 8, 64), 
-            nn.BatchNorm1d(64), 
-            nn.ReLU(inplace=True)
+            ##nn.Linear(n_filters * 2 * 8 * 8, 64), 
+            nn.Linear(n_filters*2 * 8 * 8, 128), 
+            ##nn.BatchNorm1d(64), 
+            ##nn.ReLU(inplace=True)
         ) 
         # shape = [Batch_size, 64]
         
@@ -51,12 +97,50 @@ class mlp(nn.Module):
         x = self.conv_block_2(x)
 
         x = x.view(-1, self.n_filters*2 * 8 * 8)
+        out = self.dense_block_1(x)
+        ##x = self.dense_block_2(x)
+        ##out = self.dense_block_3(x)
+
+        return out
+        
+        
+class mlp(nn.Module): 
+    def __init__(self, in_channels, out_channels, n_filters): 
+        super(mlp, self).__init__()
+        
+        self.n_filters = n_filters 
+        
+        # shape = [Batch_size, k (top k posteriors)] 
+        
+        self.dense_block_1 = nn.Sequential(
+            nn.Linear(in_channels, n_filters*2), 
+            #nn.BatchNorm1d(n_filters*2), 
+            nn.ReLU(inplace=True)
+        ) 
+        # shape = [Batch_size, n_filters*2]
+        
+        self.dense_block_2 = nn.Sequential(
+            nn.Linear(n_filters*2, n_filters*2), 
+            #nn.BatchNorm1d(n_filters*2), 
+            nn.ReLU(inplace=True)
+        ) 
+        # shape = [Batch_size, 32]
+        
+        self.dense_block_3 = nn.Sequential( 
+            nn.Linear(n_filters*2, out_channels), 
+            #nn.BatchNorm1d(out_channels), 
+            nn.Sigmoid()
+        ) 
+        # shape = [Batch_size, 10]
+        
+        
+    def forward(self, x): 
+
         x = self.dense_block_1(x)
         x = self.dense_block_2(x)
         out = self.dense_block_3(x)
 
         return out
-        
 
             
 def weights_init(m): 
@@ -65,3 +149,5 @@ def weights_init(m):
     elif isinstance(m, nn.BatchNorm2d):
         nn.init.constant_(m.weight, 1)
         nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.Linear): 
+        nn.init.xavier_normal_(m.weight.data)
