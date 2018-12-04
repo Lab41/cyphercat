b4fe761441acd09da277ef6d525c3a2bd4747349
zhampel <17304045+zhampel@users.noreply.github.com>
2018-12-04 15:56:35 -0800

    Flake8 passes for definitions.py

--- cyphercat/definitions.py
+++ cyphercat/definitions.py
@@ -1,10 +1,10 @@
 import os
 
 # Local directory of CypherCat API
-CYCAT_DIR    = os.path.dirname(os.path.abspath(__file__))
+CYCAT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Local directory containing entire repo
-REPO_DIR     = os.path.split(CYCAT_DIR)[0]
+REPO_DIR = os.path.split(CYCAT_DIR)[0]
 
 # Local directory for datasets
 DATASETS_DIR = os.path.join(REPO_DIR, 'Datasets')
