Lab41/cyphercat

commit 3114be5e874b27340b32ba423e02d8ac95ac193b

Reverts to original Libri loader and adds duplicate functions for VOiCES. Adds accuracy output for training function

Utils/train.py
@@ -34,11 +34,12 @@ def train(net, data_loader, test_loader, optimizer, criterion, n_epochs, classes
         # evaluate performance on testset at the end of each epoch
         print("[%d/%d]" %(epoch, n_epochs))
         print("Training:")
-        eval_target_net(net, data_loader, classes=classes)
+        train_accuracy = eval_target_net(net, data_loader, classes=classes)
         print("Test:")
-        eval_target_net(net, test_loader, classes=classes)
+        test_accuracy = eval_target_net(net, test_loader, classes=classes)
         #plt.plot(losses)
         #plt.show()
+        return train_accuracy, test_accuracy
         
 def train_attacker(attack_net, shadow, shadow_train, shadow_out, optimizer, criterion, n_epochs, k):
     

Attack_baselines/Membership_Inference/ml_leaks/ml-leaks_adversary1_SVCvsNN-LibriCNN.ipynb
@@ -183,7 +183,7 @@
     "\n",
     "\n",
     "#Splits data into 2 sets of speakers for target & shadow network, into above defined train:test splits\n",
-    "dfs = audio_preload_and_split(path,all_data,n_seconds,pad=False,cache=True,splits=splits, attacking = attacking, data ='Libri')  \n",
+    "dfs = Libri_preload_and_split(path,all_data,n_seconds,pad=False,cache=True,splits=splits, attacking = attacking)  \n",
     "\n",
     "#target train & test\n",
     "valid_sequence_train = LibriSpeechDataset(path, df = dfs[0], seconds = n_seconds, downsampling=1, \n",

Attack_baselines/Membership_Inference/ml_leaks/ml-leaks_adversary1_SVCvsNN-VOiCES-CNN.ipynb
@@ -9,7 +9,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 6,
+   "execution_count": 1,
    "metadata": {},
    "outputs": [
     {
@@ -70,7 +70,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 7,
+   "execution_count": 2,
    "metadata": {},
    "outputs": [],
    "source": [
@@ -82,12 +82,12 @@
     "\n",
     "transform_type = 'SFTF' #either STFT or MFCC  \n",
     "\n",
-    "data = 'VOiCES' #'Libri' or 'VOiCES'"
+    "data = 'Libri' #'Libri' or 'VOiCES'"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": 8,
+   "execution_count": 3,
    "metadata": {},
    "outputs": [],
    "source": [
@@ -131,18 +131,9 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 9,
+   "execution_count": 4,
    "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "The autoreload extension is already loaded. To reload it, use:\n",
-      "  %reload_ext autoreload\n"
-     ]
-    }
-   ],
+   "outputs": [],
    "source": [
     "n_seconds = 3\n",
     "n_epochs = 25\n",
@@ -165,7 +156,8 @@
     "%autoreload 2\n",
     "sys.path.insert(0, './../../../Utils')\n",
     "from datasets import LibriSpeechDataset\n",
-    "from datasets import audio_preload_and_split"
+    "from datasets import Libri_preload_and_split\n",
+    "from datasets import VOiCES_preload_and_split"
    ]
   },
   {
@@ -177,35 +169,21 @@
   },
   {
    "cell_type": "code",
-   "execution_count": 12,
-   "metadata": {
-    "scrolled": false
-   },
+   "execution_count": 5,
+   "metadata": {},
    "outputs": [
     {
      "name": "stdout",
      "output_type": "stream",
      "text": [
-      "Initialising VOiCES Dataset with minimum length = 3s and subsets = []\n",
-      "Finished indexing data. 899 usable files found.\n",
-      "207\n",
-      "67\n",
-      "164\n",
-      "56\n",
-      "187\n",
-      "218\n",
-      "Finished splitting data.\n",
-      "Total number of speakers:  104\n",
-      "Number of females: 48\n",
-      "Number of males: 48\n",
-      "Initialising VOiCES Dataset with minimum length = 3s and subsets = ['part_6', 'part_4', 'part_2', 'part_8', 'part_1', 'part_5', 'part_3', 'part_7']\n",
-      "Finished indexing data. 899 usable files found.\n",
-      "207\n",
-      "67\n",
-      "164\n",
-      "56\n",
-      "187\n",
-      "218\n",
+      "Initialising LibriSpeechDataset with minimum length = 3s and subsets = ['train-clean-100', 'train-clean-360']\n",
+      "Finished indexing data. 129652 usable files found.\n",
+      "29023\n",
+      "7420\n",
+      "28765\n",
+      "7352\n",
+      "28410\n",
+      "28682\n",
       "Finished splitting data.\n"
      ]
     }
@@ -220,7 +198,7 @@
     "    data_set = 'VOiCES'\n",
     "\n",
     "    #Splits data into 2 sets of speakers for target & shadow network, into above defined train:test splits\n",
-    "    dfs = audio_preload_and_split(path,all_data,n_seconds,pad=False,cache=True,splits=splits, attacking = attacking, data ='VOiCES')  \n",
+    "    dfs = VOiCES_preload_and_split(path,all_data,n_seconds,pad=False,cache=True,splits=splits, attacking = attacking)  \n",
     "\n",
     "\n",
     "    #reference files\n",
@@ -245,23 +223,7 @@
     "# import data_downloaders\n",
     "# data_downloaders._download_and_preprocess_data(path)\n",
     "\n",
-    "dfs = audio_preload_and_split(path,all_data,n_seconds,pad=False,cache=True,splits=splits, attacking = attacking, data = data)  "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 18,
-   "metadata": {
-    "scrolled": true
-   },
-   "outputs": [],
-   "source": [
-    "# Look at new VOiCES data:\n",
-    "\n",
-    "# pd.set_option('display.max_colwidth', -1)\n",
-    "# subset_index_path = path + '/index.csv'\n",
-    "# cached_df=pd.read_csv(subset_index_path)\n",
-    "# print(cached_df.filepath.to_string())"
+    "    dfs = Libri_preload_and_split(path,all_data,n_seconds,pad=False,cache=True,splits=splits, attacking = attacking)  "
    ]
   },
   {
@@ -273,7 +235,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 6,
    "metadata": {},
    "outputs": [],
    "source": [
@@ -304,7 +266,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 7,
    "metadata": {},
    "outputs": [],
    "source": [
@@ -369,9 +331,17 @@
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 8,
    "metadata": {},
-   "outputs": [],
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "325\n"
+     ]
+    }
+   ],
    "source": [
     "#Initialize NN\n",
     "\n",
@@ -389,15 +359,33 @@
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 9,
    "metadata": {
     "scrolled": false
    },
-   "outputs": [],
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[0/25]\n",
+      "Training:\n",
+      "\n",
+      "Accuracy = 63.06 %\n",
+      "\n",
+      "\n",
+      "Test:\n",
+      "\n",
+      "Accuracy = 53.64 %\n",
+      "\n",
+      "\n"
+     ]
+    }
+   ],
    "source": [
     "#Train NN\n",
     "if not pretrained:\n",
-    "    train(target_net, target_train_loader, target_test_loader, target_optim, target_loss, n_epochs, verbose = False) #classes = range(valid_sequence_test.num_speakers),\n",
+    "    train_accuracy, test_accuracy = train(target_net, target_train_loader, target_test_loader, target_optim, target_loss, n_epochs, verbose = False) #classes = range(valid_sequence_test.num_speakers),\n",
     "else:\n",
     "    fnm = 'model_weights/CNN_voice_classifier'+str(data_set)+'_target_'+transform_type+str(n_epochs-1)+'.pth'\n",
     "#     fnm = 'model_weights/CNN_voice_classifier100_target_'+str(epoch)+'.pth'\n",
@@ -408,7 +396,7 @@
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 10,
    "metadata": {},
    "outputs": [],
    "source": [
@@ -422,18 +410,30 @@
   },
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": 11,
    "metadata": {},
-   "outputs": [],
+   "outputs": [
+    {
+     "ename": "NameError",
+     "evalue": "name 'n_epoch' is not defined",
+     "output_type": "error",
+     "traceback": [
+      "\u001b[0;31m---------------------------------------------------------------------------\u001b[0m",
+      "\u001b[0;31mNameError\u001b[0m                                 Traceback (most recent call last)",
+      "\u001b[0;32m<ipython-input-11-6897a975d318>\u001b[0m in \u001b[0;36m<module>\u001b[0;34m()\u001b[0m\n\u001b[1;32m      8\u001b[0m                 \u001b[0;34m'accuracy'\u001b[0m \u001b[0;34m:\u001b[0m \u001b[0;34m[\u001b[0m\u001b[0mtrain_accuracy\u001b[0m\u001b[0;34m,\u001b[0m \u001b[0mtest_accuracy\u001b[0m\u001b[0;34m]\u001b[0m\u001b[0;34m,\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[1;32m      9\u001b[0m                 \u001b[0;34m'dataset'\u001b[0m \u001b[0;34m:\u001b[0m \u001b[0mall_data\u001b[0m\u001b[0;34m\u001b[0m\u001b[0m\n\u001b[0;32m---> 10\u001b[0;31m             }, False, filename = 'model_weights/CNN_voice_classifier'+str(data_set)+'_target_'+transform_type+str(n_epoch-1)+'.pth')\n\u001b[0m",
+      "\u001b[0;31mNameError\u001b[0m: name 'n_epoch' is not defined"
+     ]
+    }
+   ],
    "source": [
     "if not pretrained:\n",
     "\n",
-    "    save_checkpoint({\n",
-    "                'epoch': epoch,\n",
+    "    models.save_checkpoint({\n",
+    "                'epoch': n_epochs-1,\n",
     "                'arch': 'audio_CNN_classifier',\n",
     "                'state_dict': target_net.state_dict(),\n",
     "                'optimizer' : target_optim.state_dict(),\n",
-    "                'accuracy' : accuracy\n",
+    "                'accuracy' : [train_accuracy, test_accuracy],\n",
     "                'dataset' : all_data\n",
     "            }, False, filename = 'model_weights/CNN_voice_classifier'+str(data_set)+'_target_'+transform_type+str(n_epoch-1)+'.pth')"
    ]
@@ -567,7 +567,7 @@
     "# train(target_net, target_train_loader, target_test_loader, target_optim, target_loss, n_epochs, verbose = False) #classes = range(valid_sequence_test.num_speakers),\n",
     "\n",
     "if not pretrained:\n",
-    "    train(shadow_net, shadow_train_loader, shadow_test_loader, shadow_optim, shadow_loss, n_epochs, verbose = False)\n",
+    "    train_accuracy, test_accuracy = train(shadow_net, shadow_train_loader, shadow_test_loader, shadow_optim, shadow_loss, n_epochs, verbose = False)\n",
     "else:\n",
     "    fnm = 'model_weights/CNN_voice_classifier'+str(data_set)+'_shadow_'+transform_type+str(n_epochs-1)+'.pth'\n",
     "#     fnm = 'model_weights/CNN_voice_classifier100_target_'+str(epoch)+'.pth'\n",
@@ -583,12 +583,12 @@
    "outputs": [],
    "source": [
     "epoch = 24\n",
-    "save_checkpoint({\n",
+    "models.save_checkpoint({\n",
     "            'epoch': epoch,\n",
     "            'arch': 'CNN_voice_classifier',\n",
     "            'state_dict': shadow_net.state_dict(),\n",
     "            'optimizer' : shadow_optim.state_dict(),\n",
-    "            'accuracy' : accuracy\n",
+    "            'accuracy' : [train_accuracy, test_accuracy],\n",
     "            'dataset' : all_data\n",
     "        }, False, filename = 'model_weights/CNN_voice_classifier'+str(data_set)+'_shadow_'+transform_type+str(epoch)+'.pth')"
    ]
@@ -687,7 +687,6 @@
     "            'arch': 'CNN_voice_classifier',\n",
     "            'state_dict': attack_net_nn.state_dict(),\n",
     "            'optimizer' : attack_optim_nn.state_dict(),\n",
-    "            'accuracy' : accuracy\n",
     "            'dataset' : all_data\n",
     "        }, False, filename = 'model_weights/CNN_voice_classifier'+str(data_set)+'_attack_'+transform_type+str(n_epochs_attack)+'.pth')"
    ]
@@ -820,13 +819,6 @@
     "\n",
     "s"
    ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
   },
   {
    "cell_type": "code",

Utils/datasets.py
@@ -19,19 +19,9 @@ def to_categorical(y, num_classes):
     one_hot[y] = 1
     return one_hot
 
-
-def audio_preload_and_split(path,subsets,seconds,pad=False,cache=True,splits = [.8,.2], attacking = False,data = 'Libri'):
+def Libri_preload_and_split(path,subsets,seconds,pad=False,cache=True,splits = [.8,.2], attacking = False):
     fragment_seconds = seconds
-    
-    if data == 'Libri':
-        speakerF = '/LibriSpeech/SPEAKERS.TXT' #speaker info file
-    elif data == 'VOiCES':
-        speakerF = 'Lab41-SRI-VOiCES-speaker-gender-dataset_SUBSET.csv'
-    print('Initialising ' + data + ' Dataset with minimum length = {}s and subsets = {}'.format(seconds, subsets))
-
-    for file in os.listdir(path):
-        if file.startswith("part"):
-            subsets.append(file)
+    print('Initialising LibriSpeechDataset with minimum length = {}s and subsets = {}'.format(seconds, subsets))
 
     # Convert subset to list if it is a string
     # This allows to handle list of multiple subsets the same a single subset
@@ -42,63 +32,38 @@ def audio_preload_and_split(path,subsets,seconds,pad=False,cache=True,splits = [
     found_cache = {s: False for s in subsets}
     if cache:
         # Check for cached files
-        if data == 'Libri':
-            for s in subsets:
-                subset_index_path = path + '/{}.index.csv'.format(s)
-                if os.path.exists(subset_index_path):
-                    print(subset_index_path)
-                    cached_df.append(pd.read_csv(subset_index_path))
-                    found_cache[s] = True
-        elif data == 'VOiCES':
-            subset_index_path = path + '/index.csv'
+        for s in subsets:
+            subset_index_path = path + '/{}.index.csv'.format(s)
             if os.path.exists(subset_index_path):
                 cached_df.append(pd.read_csv(subset_index_path))
-                for s in subsets:
-                    found_cache[s] = True
-        
+                found_cache[s] = True
 
     # Index the remaining subsets if any
     if all(found_cache.values()) and cache:
         df = pd.concat(cached_df)
     else:
-        if data == 'Libri':
-            df = pd.read_csv(path+speakerF, skiprows=11, delimiter='|', error_bad_lines=False)
-            df.columns = [col.strip().replace(';', '').lower() for col in df.columns]
-            df = df.assign(
-                sex=df['sex'].apply(lambda x: x.strip()),
-                subset=df['subset'].apply(lambda x: x.strip()),
-                name=df['name'].apply(lambda x: x.strip()),
-            )
-        elif data == 'VOiCES':
-            df = pd.read_csv(path+speakerF, skiprows=0, delimiter=',', error_bad_lines=False)
-            df.columns = [col.strip().replace(';', '').lower() for col in df.columns]
-            df = df.rename(columns={'speaker': 'id', 'gender': 'sex','dataset':'subset'})
-            df = df.assign(
-                sex=df['sex'].apply(lambda x: x.strip()),
-                subset=df['subset'].apply(lambda x: x.strip()),
-            )
-        
+        df = pd.read_csv(path+'/LibriSpeech/SPEAKERS.TXT', skiprows=11, delimiter='|', error_bad_lines=False)
+        df.columns = [col.strip().replace(';', '').lower() for col in df.columns]
+        df = df.assign(
+            sex=df['sex'].apply(lambda x: x.strip()),
+            subset=df['subset'].apply(lambda x: x.strip()),
+            name=df['name'].apply(lambda x: x.strip()),
+        )
+
         audio_files = []
         for subset, found in found_cache.items():
             if not found:
-                audio_files += index_subset(path, subset,data)
-                
-       
+                audio_files += index_subset(path, subset)
+
         # Merge individual audio files with indexing dataframe
         df = pd.merge(df, pd.DataFrame(audio_files))
-       
+
         # # Concatenate with already existing dataframe if any exist
-        
         df = pd.concat(cached_df+[df])
-    
 
     # Save index files to data folder
-    if data == 'Libri':
-        for s in subsets:
-
-            df[df['subset'] == s].to_csv(path + '/{}.index.csv'.format(s), index=False)
-    else:
-        df.to_csv(path + '/index.csv', index=False)
+    for s in subsets:
+        df[df['subset'] == s].to_csv(path + '/{}.index.csv'.format(s), index=False)
 
     # Trim too-small files
     if not pad:
@@ -106,9 +71,7 @@ def audio_preload_and_split(path,subsets,seconds,pad=False,cache=True,splits = [
     num_speakers = len(df['id'].unique())
 
     # Renaming for clarity
-    df = df.rename(columns={'id': 'speaker_id'})
-    if data == 'Libri':
-        df = df.rename(columns={'minutes': 'speaker_minutes'})
+    df = df.rename(columns={'id': 'speaker_id', 'minutes': 'speaker_minutes'})
 
     # Index of dataframe has direct correspondence to item in dataset
     df = df.reset_index(drop=True)
@@ -161,7 +124,7 @@ def audio_preload_and_split(path,subsets,seconds,pad=False,cache=True,splits = [
 
     return dfs
 
-def index_subset(path , subset, data):
+def index_subset(path , subset):
     """
     Index a subset by looping through all of it's files and recording their speaker ID, filepath and length.
     :param subset: Name of the subset
@@ -172,34 +135,21 @@ def index_subset(path , subset, data):
     print('Indexing {}...'.format(subset))
     # Quick first pass to find total for tqdm bar
     subset_len = 0
-    if data == 'Libri':
-        addpath = '/LibriSpeech/'
-        ftype = '.flac'
-    else:
-        addpath = ''
-        ftype = '.wav'
-                    
-   
-                    
-    for root, folders, files in os.walk(path + addpath +'{}/'.format(subset)):
-        subset_len += len([f for f in files if f.endswith(ftype)])
-       
+    for root, folders, files in os.walk(path + '/LibriSpeech/{}/'.format(subset)):
+        subset_len += len([f for f in files if f.endswith('.flac')])
+
     progress_bar = tqdm(total=subset_len)
-    for root, folders, files in os.walk(path + addpath + '{}/'.format(subset)):
+    for root, folders, files in os.walk(path + '/LibriSpeech/{}/'.format(subset)):
         if len(files) == 0:
             continue
-        if data == 'Libri':
-            librispeech_id = int(root.split('/')[-2])
-            
+
+        librispeech_id = int(root.split('/')[-2])
 
         for f in files:
             # Skip non-sound files
-            if not f.endswith(ftype):
+            if not f.endswith('.flac'):
                 continue
-                     
-            if data == 'VOiCES':
-                librispeech_id = int(f[f.index('sp')+2:f.index('sp')+6])
-                
+
             progress_bar.update(1)
 
             instance, samplerate = sf.read(os.path.join(root, f))
@@ -214,8 +164,175 @@ def index_subset(path , subset, data):
     progress_bar.close()
     return audio_files
 
+def VOiCES_preload_and_split(path,subsets,seconds,pad=False,cache=True,splits = [.8,.2], attacking = False):
+    fragment_seconds = seconds
     
+    speakerF = 'Lab41-SRI-VOiCES-speaker-gender-dataset_SUBSET.csv'
+    print('Initialising VOiCES Dataset with minimum length = {}s and subsets = {}'.format(seconds, subsets))
+
+    for file in os.listdir(path):
+        if file.startswith("part"):
+            subsets.append(file)
+
+    # Convert subset to list if it is a string
+    # This allows to handle list of multiple subsets the same a single subset
+    if isinstance(subsets, str):
+        subsets = [subsets]
+
+    cached_df = []
+    found_cache = {s: False for s in subsets}
+    if cache:
+        # Check for cached files
+#         if data == 'Libri':
+#             for s in subsets:
+#                 subset_index_path = path + '/{}.index.csv'.format(s)
+#                 if os.path.exists(subset_index_path):
+#                     print(subset_index_path)
+#                     cached_df.append(pd.read_csv(subset_index_path))
+#                     found_cache[s] = True
+       
+        subset_index_path = path + '/index.csv'
+        if os.path.exists(subset_index_path):
+            cached_df.append(pd.read_csv(subset_index_path))
+            for s in subsets:
+                found_cache[s] = True
+
+
+    # Index the remaining subsets if any
+    if all(found_cache.values()) and cache:
+        df = pd.concat(cached_df)
+    else:
+        df = pd.read_csv(path+speakerF, skiprows=0, delimiter=',', error_bad_lines=False)
+        df.columns = [col.strip().replace(';', '').lower() for col in df.columns]
+        df = df.rename(columns={'speaker': 'id', 'gender': 'sex','dataset':'subset'})
+        df = df.assign(
+            sex=df['sex'].apply(lambda x: x.strip()),
+            subset=df['subset'].apply(lambda x: x.strip()),
+        )
         
+        audio_files = []
+        for subset, found in found_cache.items():
+            if not found:
+                audio_files += index_subset_VOiCES(path, subset)
+                
+       
+        # Merge individual audio files with indexing dataframe
+        df = pd.merge(df, pd.DataFrame(audio_files))
+       
+        # # Concatenate with already existing dataframe if any exist
+        
+        df = pd.concat(cached_df+[df])
+    
+
+    # Save index files to data folder
+#     if data == 'Libri':
+#         for s in subsets:
+#             df[df['subset'] == s].to_csv(path + '/{}.index.csv'.format(s), index=False)  
+    df.to_csv(path + '/index.csv', index=False)
+
+    # Trim too-small files
+    if not pad:
+        df = df[df['seconds'] > fragment_seconds]
+    num_speakers = len(df['id'].unique())
+
+    # Renaming for clarity
+    df = df.rename(columns={'id': 'speaker_id'})
+    
+    # Index of dataframe has direct correspondence to item in dataset
+    df = df.reset_index(drop=True)
+    df = df.assign(id=df.index.values)
+
+    # Convert arbitrary integer labels of dataset to ordered 0-(num_speakers - 1) labels
+    unique_speakers = sorted(df['speaker_id'].unique())
+
+    print('Finished indexing data. {} usable files found.'.format(len(df)))
+
+    dfs = {} #dictionary of dataframes
+        
+        
+    #split df into data-subsets
+    if attacking == 1: #adversary 1, which requires an additional split for a shadow network
+        #splits unique speakers into three unequal parts. 
+        # num speakers for train & test is the same.
+        # the below was solved with a system of equations
+        n = int(num_speakers//(2+2*splits[0]))#amt data depends on amt train dat
+        #n is train data for shadow & target networks
+        
+        unique_speakers1 = unique_speakers[:n] #target 
+        unique_speakers2 = unique_speakers[n:2*n] # shadow
+        unique_speakers3 = unique_speakers[2*n:] # out (target + shadow)
+
+        dfs = splitter(dfs,df,unique_speakers1, splits,0)
+        dfs = splitter(dfs,df,unique_speakers2, splits,2)
+        dfs = splitter(dfs,df,unique_speakers3, splits=[0.5,0.5],N = 4) #split out data for attack train  + test evenly
+        
+    elif attacking == 3: #adversary 3, which just requires in & out data
+        #splits unique speakers into two unequal parts. 
+        # the below was solved with a system of equations
+        n = int(num_speakers//(1+splits[0]))#amt data depends on amt train dat
+        #n is train data for target networks
+        
+        unique_speakers1 = unique_speakers[:n] #target 
+        unique_speakers2 = unique_speakers[n:] # out (target + shadow)
+
+        dfs = splitter(dfs,df,unique_speakers1, splits,0)
+        dfs = splitter(dfs,df,unique_speakers2, splits=[1,0],N=2) #split out data for just attack eval
+    else: # just split into train & test
+        dfs = splitter(dfs, df,unique_speakers, splits, 0)
+ 
+    #check that the splits were as desired:
+        
+    for d in dfs:
+        print(len(dfs[d]))
+
+    print('Finished splitting data.')
+
+    return dfs
+
+def index_subset_VOiCES(path , subset):
+    """
+    Index a subset by looping through all of it's files and recording their speaker ID, filepath and length.
+    :param subset: Name of the subset
+    :return: A list of dicts containing information about all the audio files in a particular subset of the
+    LibriSpeech dataset
+    """
+    audio_files = []
+    print('Indexing {}...'.format(subset))
+    # Quick first pass to find total for tqdm bar
+    subset_len = 0
+    addpath = ''
+    ftype = '.wav'
+
+    for root, folders, files in os.walk(path + addpath +'{}/'.format(subset)):
+        subset_len += len([f for f in files if f.endswith(ftype)])
+       
+    progress_bar = tqdm(total=subset_len)
+    for root, folders, files in os.walk(path + addpath + '{}/'.format(subset)):
+        if len(files) == 0:
+            continue
+        
+        for f in files:
+            # Skip non-sound files
+            if not f.endswith(ftype):
+                continue
+                     
+            
+            librispeech_id = int(f[f.index('sp')+2:f.index('sp')+6])
+                
+            progress_bar.update(1)
+
+            instance, samplerate = sf.read(os.path.join(root, f))
+
+            audio_files.append({
+                'id': librispeech_id,
+                'filepath': os.path.join(root, f),
+                'length': len(instance),
+                'seconds': len(instance) * 1. / LIBRISPEECH_SAMPLING_RATE
+            })
+
+    progress_bar.close()
+    return audio_files
+      
 def splitter(dfs,df,unique_speakers, splits,N):
     #N is to keep track of the dataframe dict keys
     n_splits = len(splits)

commit 0f13fbce5e202dcc28cc3f00b599ca481539f710

Fixes bug introduced in commit 3114be5e874b27340b32ba423e02d8ac95ac193b

Utils/train.py
@@ -39,7 +39,7 @@ def train(net, data_loader, test_loader, optimizer, criterion, n_epochs, classes
         test_accuracy = eval_target_net(net, test_loader, classes=classes)
         #plt.plot(losses)
         #plt.show()
-        return train_accuracy, test_accuracy
+    return train_accuracy, test_accuracy
         
 def train_attacker(attack_net, shadow, shadow_train, shadow_out, optimizer, criterion, n_epochs, k):
     