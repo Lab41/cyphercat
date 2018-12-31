74b6775ed87800f7416ca7a6e5a5cf09cc238eb4
Lucas Tindall <ltindall@ucsd.edu>
2018-12-31 00:37:06 -0800

    Added notebook for ML-Leaks adversary 2 (Cifar10/LFW)

--- Utils/models.py
+++ Utils/models.py
@@ -95,9 +95,16 @@ class tiny_cnn(nn.Module):
         
         return out
     
+def calc_mlleaks_cnn_size(size): 
+    x = new_size_conv(size, 5,1,2)
+    x = new_size_max_pool(x,2,2)
+    x = new_size_conv(x,5,1,2)
+    out = new_size_max_pool(x,2,2)
     
+    return out
+
 class mlleaks_cnn(nn.Module): 
-    def __init__(self, n_in=3, n_out=10, n_hidden=64): 
+    def __init__(self, n_in=3, n_out=10, n_hidden=64, size=32): 
         super(mlleaks_cnn, self).__init__()
         
         self.n_hidden = n_hidden 
@@ -114,13 +121,15 @@ class mlleaks_cnn(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)
         ) 
-        self.fc = nn.Linear(2*n_hidden * 8 * 8, 128)
+
+        fc_feature_size = calc_mlleaks_cnn_size(size)
+        self.fc = nn.Linear(int(2*n_hidden * fc_feature_size * fc_feature_size), 128)
         self.output = nn.Linear(2*n_hidden, n_out)
         
     def forward(self, x): 
         x = self.conv_block_1(x)
         x = self.conv_block_2(x)
-        x = x.view(-1, 2*self.n_hidden * 8 * 8)
+        x = x.view(x.size(0), -1)
         x = self.fc(x)
         out = self.output(x)
         
